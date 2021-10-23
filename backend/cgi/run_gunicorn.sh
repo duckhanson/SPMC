c9416d35bf256aa6f08ae7983eb2729d6fa11ab3
Lucas Lu <ducklyu0301@gmail.com>
2021-10-23 18:06:10 +0800

    Change Sever Port

--- backend/cgi/run_gunicorn.sh
+++ backend/cgi/run_gunicorn.sh
@@ -1,1 +1,1 @@
-gunicorn --bind 0.0.0.0:5000 wsgi:app
+gunicorn --bind 0.0.0.0:3333 wsgi:app
--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    API_LOCATION: 'http://localhost:5000/'
+    API_LOCATION: 'http://localhost:3333/'
 }
